93949e2608d455c56a008610291d61f667a2f30e
Shilpa S Pillai <shilpaspillaitvla@gmail.com>
2024-03-01 15:37:47 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+/* left sidebar onclick */
+function showSidebar()
+{
+    const sidebar=document.querySelector(".sidebar");
+    sidebar.style.display="flex";
+}
+function hideSidebar()
+{
+   const sidebar=document.querySelector(".sidebar");
+    sidebar.style.display="none";
+    
+    
+}
+
+/*right sidebar onclick */
+ function showRightsidebar()
+ {
+   const rightsidebar=document.querySelector(".Rightsidebar");
+   rightsidebar.style.display="flex";
+//    
+ }
+ function hideRightsidebar()
+ {
+    const rightsidebar=document.querySelector(".Rightsidebar");
+    rightsidebar.style.display="none";
+    
+
+ }
+ 
